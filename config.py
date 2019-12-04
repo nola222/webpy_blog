990269f70b8a8b95491ba4727aa9b8d64ee36e70
lixianchun <lixianchun@cyai.com>
2019-12-04 12:01:11 +0800

    add config

---
+++ config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+时间: 2019/12/4 11:28
+
+作者: nola2
+
+更改记录:
+
+重要说明:
+"""
+
+DBN = "mysql",
+HOST = "127.0.0.1",
+PORT = 3306,
+USER = "root",
+PW = "root",
+DB = "blog"
