becb7ab03f95fafe805d4aced588b541dd2399d5
lixianchun <lixianchun@cyai.com>
2019-12-04 14:19:13 +0800

    update db init sql

--- db_init.sql
+++ db_init.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `entries` (
-  `id` int(0) NOT NULL COMMENT 'id',
+CREATE TABLE `blog`.`entries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `title` tinytext NOT NULL COMMENT '博客标题',
   `content` text NOT NULL COMMENT '博客内容',
   `posted_on` datetime(0) NOT NULL COMMENT '博客发布时间',
